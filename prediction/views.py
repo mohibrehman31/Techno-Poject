ea04c41a25d9486f6e23a5e48a145cb96d0d53b6
Mohib Rehman <mohibrehman31@gmail.com>
2022-07-26 12:02:15 +0000

    m

--- prediction/views.py
+++ prediction/views.py
@@ -16,7 +16,7 @@ def predict(request):
         acity = request.POST['Arrival_city'].lower()
         cabin = request.POST['Cabin']
         date = request.POST['Date']
-        time = request.POST['Time']
+        time = request.POST['Time'].split(':')[0]
         month = pd.to_datetime(date).month
         hour = time
         day = pd.to_datetime(date).day
@@ -33,198 +33,65 @@ def predict(request):
         day = np.asarray(day)
         hour = np.asarray(hour)
 
-        with zipfile.ZipFile('B/B_price_predict.zip', 'r') as zip_ref:
-            zip_ref.extractall('B/')
-
-        with zipfile.ZipFile('B/B_time_predict.zip', 'r') as zip_ref1:
-            zip_ref1.extractall('B/')
-
-        with zipfile.ZipFile('E/E_price_predict.zip', 'r') as zip_ref2:
-            zip_ref2.extractall('E/')
-
-        with zipfile.ZipFile('E/E_time_predict.zip', 'r') as zip_ref3:
-            zip_ref3.extractall('E/')
-
-        with zipfile.ZipFile('PE/PE_price_predict.zip', 'r') as zip_ref4:
-            zip_ref4.extractall('PE/')
-
-        with zipfile.ZipFile('PE/PE_time_predict.zip', 'r') as zip_ref5:
-            zip_ref5.extractall('PE/')
-
         if cabin == 'B':
             print("HII_B")
-            with open("B/B_price_predict.pkl", "rb") as f:
-                model_price = pickle.load(f)
-            with open("B/B_time_predict.pkl", "rb") as f:
-                model_time = pickle.load(f)
-            with open("B/B_airline_dict.pkl", "rb") as f:
-                airline_dict = pickle.load(f)
-            with open("B/B_arrival_city_dict.pkl", "rb") as f:
-                arrival_dict = pickle.load(f)
-            with open("B/B_dept_city_dict.pkl", "rb") as f:
-                dept_dict = pickle.load(f)
-            sc = load("B/B_Dept_date_std_scaler.bin")
-            Dept_date = sc.transform(day.reshape(-1, 1))
-
-            sc = load("B/B_dept_hours_std_scaler.bin")
-            dept_hours = sc.transform(hour.reshape(-1, 1))
-
-            with open('B/B_duration.pkl', "rb") as f:
-                duration = pkl.load(f)
-
-            k_0 = dcity+","+acity+" ,"+str(0)
-            k_1 = dcity+","+acity+" ,"+str(1)
+            model_time = joblib.load('B/B_time_predict.pkl')
+            price_scaler = joblib.load('B/B_price_scaler.pkl')
+            model_price = joblib.load('B/B_price_predict.pkl')
+            scaler = joblib.load('B/B_scaler.pkl')
+            airline_dict = joblib.load('B/B_airline_dict.pkl')
+            city_dict = joblib.load('B/B_City_dict.pkl')
+            duration = joblib.load('B/B_duration.pkl')
+            acity_enc = city_dict[acity]
+            dcity_enc = city_dict[dcity]
+            k_0 = str(dcity_enc)+","+str(acity_enc)
             k_0 = np.asarray(duration[k_0])
-            k_1 = np.asarray(duration[k_1])
-            sc = load('B/B_duration_std_scaler.bin')
-            scale_0 = sc.transform(k_0.reshape(-1, 1))
-            sc = load('B/B_duration_std_scaler.bin')
-            scale_1 = sc.transform(k_1.reshape(-1, 1))
-            for j in range(2):
-                p_0 = model_time.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 0, scale_0, weekday, dept_hours, temp_time]])
-
-            for j in range(2):
-                p_1 = model_time.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 1, scale_1, weekday, dept_hours, temp_time]])
-
-            sc = load('B/B_price_dept_hours_std_scaler.bin')
-            h_0 = sc.transform(p_0.reshape(-1, 1))
-            h_1 = sc.transform(p_1.reshape(-1, 1))
-
-            headings = ("Airline", "Stops", "Source_city",
-                        "Arrival_city", "Duration", "Optimal_time", "Price")
-            data = []
-
-            for j in range(2):
-                pred = model_price.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 0, scale_0, weekday, h_0, temp_time]])
-                data.append([airline_dict[j].capitalize(), 0, dcity.capitalize(
-                ), acity.capitalize(), k_0, int(p_0[0]), "₹"+str(int(pred[0]))])
-
-            for j in range(2):
-                pred = model_price.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 1, scale_1, weekday, h_1, temp_time]])
-                data.append([airline_dict[j].capitalize(), 1, dcity.capitalize(
-                ), acity.capitalize(), k_1, int(p_1[0]), "₹"+str(int(pred[0]))])
-            data = tuple(data)
+            x = scaler.transform([[1 ,dcity_enc,day,acity_enc,k_0,weekday,hour,temp_time]])
+            optimal_time = model_time.predict(x)
+            y = price_scaler.transform([[1 ,dcity_enc,day,acity_enc,k_0,weekday,hour,temp_time,optimal_time]])
+            optimal_price = model_price.predict(y)
+            print('Optimal hours = ',optimal_time,'Optimal Price = ',optimal_price)
+            print("HII_B_END")
 
         if cabin == 'PE':
-            with open("PE/PE_price_predict.pkl", "rb") as f:
-                model_price = pickle.load(f)
-            with open("PE/PE_time_predict.pkl", "rb") as f:
-                model_time = pickle.load(f)
-            with open("PE/PE_airline_dict.pkl", "rb") as f:
-                airline_dict = pickle.load(f)
-            with open("PE/PE_arrival_city_dict.pkl", "rb") as f:
-                arrival_dict = pickle.load(f)
-            with open("PE/PE_dept_city_dict.pkl", "rb") as f:
-                dept_dict = pickle.load(f)
-            sc = load("PE/PE_Dept_date_std_scaler.bin")
-            Dept_date = sc.transform(day.reshape(-1, 1))
-
-            sc = load("PE/PE_dept_hours_std_scaler.bin")
-            dept_hours = sc.transform(hour.reshape(-1, 1))
-
-            with open('PE/PE_duration.pkl', "rb") as f:
-                duration = pkl.load(f)
-
-            k_0 = dcity+","+acity+" ,"+str(0)
-            k_1 = dcity+","+acity+" ,"+str(1)
+            print("HII_PE")
+            model_time = joblib.load('PE/PE_time_predict.pkl')
+            price_scaler = joblib.load('PE/PE_price_scaler.pkl')
+            model_price = joblib.load('PE/PE_price_predict.pkl')
+            scaler = joblib.load('PE/PE_scaler.pkl')
+            airline_dict = joblib.load('PE/PE_airline_dict.pkl')
+            city_dict = joblib.load('PE/PE_City_dict.pkl')
+            duration = joblib.load('PE/PE_duration.pkl')
+            acity_enc = city_dict[acity]
+            dcity_enc = city_dict[dcity]
+            k_0 = str(dcity_enc)+","+str(acity_enc)
             k_0 = np.asarray(duration[k_0])
-            k_1 = np.asarray(duration[k_1])
-            sc = load('PE/PE_duration_std_scaler.bin')
-            scale_0 = sc.transform(k_0.reshape(-1, 1))
-            sc = load('PE/PE_duration_std_scaler.bin')
-            scale_1 = sc.transform(k_1.reshape(-1, 1))
-
-            for j in range(2):
-                p_0 = model_time.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 0, scale_0, weekday, dept_hours, temp_time]])
-
-            for j in range(2):
-                p_1 = model_time.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 1, scale_1, weekday, dept_hours, temp_time]])
-
-            sc = load('PE/PE_price_dept_hours_std_scaler.bin')
-            h_0 = sc.transform(p_0.reshape(-1, 1))
-            h_1 = sc.transform(p_1.reshape(-1, 1))
-
-            headings = ("Airline", "Stops", "Source_city",
-                        "Arrival_city", "Duration", "Optimal_time", "Price")
-            data = []
-            for j in range(2):
-                pred = model_price.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 0, scale_0, weekday, h_0, temp_time]])
-                data.append([airline_dict[j].capitalize(), 0, dcity.capitalize(
-                ), acity.capitalize(), k_0, int(p_0[0]), "₹"+str(int(pred[0]))])
-
-            for j in range(2):
-                pred = model_price.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 1, scale_1, weekday, h_1, temp_time]])
-                data.append([airline_dict[j].capitalize(), 1, dcity.capitalize(
-                ), acity.capitalize(), k_1, int(p_1[0]), "₹"+str(int(pred[0]))])
-            data = tuple(data)
+            x = scaler.transform([[1 ,dcity_enc,day,acity_enc,k_0,weekday,hour,temp_time]])
+            optimal_time = model_time.predict(x)
+            y = price_scaler.transform([[1 ,dcity_enc,day,acity_enc,k_0,weekday,hour,temp_time,optimal_time]])
+            optimal_price = model_price.predict(y)
+            print('Optimal hours = ',optimal_time,'Optimal Price = ',optimal_price)
+            print("HII_PE_END")
 
         if cabin == 'E':
             print("HII_E")
-            with open("E_price_predict.pkl", "rb") as f:
-                model_price = pickle.load(f)
-            with open("E_time_predict.pkl", "rb") as f:
-                model_time = pickle.load(f)
-            with open("E_airline_dict.pkl", "rb") as f:
-                airline_dict = pickle.load(f)
-            with open("E_arrival_city_dict.pkl", "rb") as f:
-                arrival_dict = pickle.load(f)
-            with open("E_dept_city_dict.pkl", "rb") as f:
-                dept_dict = pickle.load(f)
-            sc = load("E_Dept_date_std_scaler.bin")
-            Dept_date = sc.transform(day.reshape(-1, 1))
-
-            sc = load("E/E_dept_hours_std_scaler.bin")
-            dept_hours = sc.transform(hour.reshape(-1, 1))
-
-            with open('E/E_duration.pkl', "rb") as f:
-                duration = pkl.load(f)
-
-            k_0 = dcity+","+acity+" ,"+str(0)
-            k_1 = dcity+","+acity+" ,"+str(1)
+            model_time = joblib.load('E/E_time_predict.pkl')
+            price_scaler = joblib.load('E/E_price_scaler.pkl')
+            model_price = joblib.load('E/E_price_predict.pkl')
+            scaler = joblib.load('E/E_scaler.pkl')
+            airline_dict = joblib.load('E/E_airline_dict.pkl')
+            city_dict = joblib.load('E/E_City_dict.pkl')
+            duration = joblib.load('E_duration.pkl')
+            acity_enc = city_dict[acity]
+            dcity_enc = city_dict[dcity]
+            k_0 = str(dcity_enc)+","+str(acity_enc)
             k_0 = np.asarray(duration[k_0])
-            k_1 = np.asarray(duration[k_1])
-            sc = load('E/E_duration_std_scaler.bin')
-            scale_0 = sc.transform(k_0.reshape(-1, 1))
-            sc = load('E/E_duration_std_scaler.bin')
-            scale_1 = sc.transform(k_1.reshape(-1, 1))
+            x = scaler.transform([[1 ,dcity_enc,day,acity_enc,k_0,weekday,hour,temp_time]])
+            optimal_time = model_time.predict(x)
+            y = price_scaler.transform([[1 ,dcity_enc,day,acity_enc,k_0,weekday,hour,temp_time,optimal_time]])
+            optimal_price = model_price.predict(y)
+            print('Optimal hours = ',optimal_time,'Optimal Price = ',optimal_price)
             print("HII_E_END")
-
-            for j in range(2):
-                p_0 = model_time.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 0, scale_0, weekday, dept_hours, temp_time]])
-
-            for j in range(2):
-                p_1 = model_time.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 1, scale_1, weekday, dept_hours, temp_time]])
-
-            sc = load('E/E_price_dept_hours_std_scaler.bin')
-            h_0 = sc.transform(p_0.reshape(-1, 1))
-            h_1 = sc.transform(p_1.reshape(-1, 1))
-
-            headings = ("Airline", "Stops", "Source_city",
-                        "Arrival_city", "Duration", "Optimal_time", "Price")
-            data = []
-
-            for j in range(5):
-                pred = model_price.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 0, scale_0, weekday, h_0, temp_time]])
-                data.append([airline_dict[j].capitalize(), 0, dcity.capitalize(
-                ), acity.capitalize(), k_0, int(p_0[0]), "₹"+str(int(pred[0]))])
-
-            for j in range(5):
-                pred = model_price.predict(
-                    [[j, dept_dict[dcity], Dept_date, arrival_dict[acity], 1, scale_1, weekday, h_1, temp_time]])
-                data.append([airline_dict[j].capitalize(), 1, dcity.capitalize(
-                ), acity.capitalize(), k_1, int(p_1[0]), "₹"+str(int(pred[0]))])
-            data = tuple(data)
 
     return render(request, 'index.html')
 
